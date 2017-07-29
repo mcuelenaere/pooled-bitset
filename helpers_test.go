8b649179a414ad84701c2c39d0998b0ffd901d60
Maurus Cuelenaere <mcuelenaere@gmail.com>
2017-07-29 17:15:48 +0200

    Use official Go subtests instead of trying to mimic them

--- helpers_test.go
+++ helpers_test.go
@@ -1,42 +1,43 @@
 package pooled_bitset
 
 import (
+	"fmt"
 	"math/rand"
 	"reflect"
 	"runtime"
 	"testing"
 )
 
-var popCountSliceTestCases = []struct {
-	Input    []uint64
-	Expected uint64
-}{
-	{[]uint64{}, 0},
-	{[]uint64{0x1}, 1},
-	{[]uint64{0x2}, 1},
-	{[]uint64{0x44}, 2},
-	{[]uint64{0x8000000000000000}, 1},
-	{[]uint64{0xFFFFFFFFFFFFFFFF}, 64},
-
-	{[]uint64{0x1, 0x2}, 2},
-	{[]uint64{0x8000000000000000, 0xFFFFFFFFFFFFFFFF}, 65},
-}
-
-func TestPopCountSliceGeneric(t *testing.T) {
-	for _, testCase := range popCountSliceTestCases {
-		output := popcountSliceGeneric(testCase.Input)
-		if output != testCase.Expected {
-			t.Errorf("popCountSliceGeneric() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
-		}
-	}
-}
-
 func TestPopCountSlice(t *testing.T) {
-	for _, testCase := range popCountSliceTestCases {
-		output := popcountSlice(testCase.Input)
-		if output != testCase.Expected {
-			t.Errorf("popcountSliceAsm() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
-		}
+	testCases := []struct {
+		Input    []uint64
+		Expected uint64
+	}{
+		{[]uint64{}, 0},
+		{[]uint64{0x1}, 1},
+		{[]uint64{0x2}, 1},
+		{[]uint64{0x44}, 2},
+		{[]uint64{0x8000000000000000}, 1},
+		{[]uint64{0xFFFFFFFFFFFFFFFF}, 64},
+
+		{[]uint64{0x1, 0x2}, 2},
+		{[]uint64{0x8000000000000000, 0xFFFFFFFFFFFFFFFF}, 65},
+	}
+
+	for idx, testCase := range testCases {
+		t.Run(fmt.Sprintf("version=generic/index=%d", idx), func (t *testing.T) {
+			output := popcountSliceGeneric(testCase.Input)
+			if output != testCase.Expected {
+				t.Errorf("popCountSliceGeneric() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
+			}
+		})
+
+		t.Run(fmt.Sprintf("version=asm/index=%d", idx), func (t *testing.T) {
+			output := popcountSlice(testCase.Input)
+			if output != testCase.Expected {
+				t.Errorf("popcountSliceAsm() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
+			}
+		})
 	}
 }
 
@@ -69,32 +70,32 @@ func BenchmarkPopCountSlice(b *testing.B) {
 	}
 }
 
-var findFirstSetBitTestCases = []struct {
-	Input    uint64
-	Expected uint64
-}{
-	{0x1, 0},
-	{0x2, 1},
-	{0x44, 2},
-	{0x8000000000000000, 63},
-	{0xFFFFFFFFFFFFFFFF, 0},
-}
-
-func TestFindFirstSetBitGeneric(t *testing.T) {
-	for _, testCase := range findFirstSetBitTestCases {
-		output := findFirstSetBitGeneric(testCase.Input)
-		if output != testCase.Expected {
-			t.Errorf("findFirstSetBitGeneric() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
-		}
-	}
-}
-
 func TestFindFirstSetBit(t *testing.T) {
-	for _, testCase := range findFirstSetBitTestCases {
-		output := findFirstSetBit(testCase.Input)
-		if output != testCase.Expected {
-			t.Errorf("findFirstSetBitAsm() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
-		}
+	testCases := []struct {
+		Input    uint64
+		Expected uint64
+	}{
+		{0x1, 0},
+		{0x2, 1},
+		{0x44, 2},
+		{0x8000000000000000, 63},
+		{0xFFFFFFFFFFFFFFFF, 0},
+	}
+
+	for idx, testCase := range testCases {
+		t.Run(fmt.Sprintf("version=generic/index=%d", idx), func (t *testing.T) {
+			output := findFirstSetBitGeneric(testCase.Input)
+			if output != testCase.Expected {
+				t.Errorf("findFirstSetBitGeneric() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
+			}
+		})
+
+		t.Run(fmt.Sprintf("version=asm/index=%d", idx), func (t *testing.T) {
+			output := findFirstSetBit(testCase.Input)
+			if output != testCase.Expected {
+				t.Errorf("findFirstSetBitAsm() gave %d instead of expected %d for input 0x%x", output, testCase.Expected, testCase.Input)
+			}
+		})
 	}
 }
 
@@ -113,52 +114,43 @@ func BenchmarkFindFirstSetBitAsm(b *testing.B) {
 	}
 }
 
-var bitOpsSliceTestCases = []struct {
-	InputA      []uint64
-	InputB      []uint64
-	ExpectedAnd []uint64
-	ExpectedOr  []uint64
-	ExpectedXor []uint64
-}{
-	{[]uint64{}, []uint64{}, []uint64{}, []uint64{}, []uint64{}},
-	{[]uint64{0x1}, []uint64{0x0}, []uint64{0x0}, []uint64{0x1}, []uint64{0x1}},
-	{[]uint64{0x1, 0x1, 0x1, 0x1}, []uint64{0x0, 0x1, 0x0, 0x0}, []uint64{0x0, 0x1, 0x0, 0x0}, []uint64{0x1, 0x1, 0x1, 0x1}, []uint64{0x1, 0x0, 0x1, 0x1}},
-}
+func TestBitOpSlice(t *testing.T) {
+	configurations := []struct {
+		Operator string
+		Version string
+		Func func(dest, a, b []uint64)
+	}{
+		{"AND", "generic", andSliceGeneric},
+		{"AND", "asm", andSlice},
+		{"OR", "generic", orSliceGeneric},
+		{"OR", "asm", orSlice},
+		{"XOR", "generic", xorSliceGeneric},
+		{"XOR", "asm", xorSlice},
+	}
 
-const (
-	OP_AND = iota
-	OP_OR
-	OP_XOR
-)
+	testCases := []struct {
+		InputA      []uint64
+		InputB      []uint64
+		Expected    map[string][]uint64
+	}{
+		{[]uint64{}, []uint64{}, map[string][]uint64{"AND": []uint64{}, "OR": []uint64{}, "XOR": []uint64{}}},
+		{[]uint64{0x1}, []uint64{0x0}, map[string][]uint64{"AND": []uint64{0x0}, "OR": []uint64{0x1}, "XOR": []uint64{0x1}}},
+		{[]uint64{0x1, 0x1, 0x1, 0x1}, []uint64{0x0, 0x1, 0x0, 0x0}, map[string][]uint64{"AND": []uint64{0x0, 0x1, 0x0, 0x0}, "OR": []uint64{0x1, 0x1, 0x1, 0x1}, "XOR": []uint64{0x1, 0x0, 0x1, 0x1}}},
+	}
 
-func testBitOpSlice(t *testing.T, bitOpFunc func(dest, a, b []uint64), bitOp int) {
-	for _, testCase := range bitOpsSliceTestCases {
-		output := make([]uint64, len(testCase.InputA))
-		var expected []uint64
-		switch bitOp {
-		case OP_AND:
-			expected = testCase.ExpectedAnd
-		case OP_OR:
-			expected = testCase.ExpectedOr
-		case OP_XOR:
-			expected = testCase.ExpectedXor
-		}
-
-		bitOpFunc(output, testCase.InputA, testCase.InputB)
-		if !reflect.DeepEqual(output, expected) {
-			t.Errorf("%s gave %v instead of expected %v for inputs %v and %v", runtime.FuncForPC(reflect.ValueOf(bitOpFunc).Pointer()).Name(), output, expected, testCase.InputA, testCase.InputB)
+	for _, configuration := range configurations {
+		for idx, testCase := range testCases {
+			t.Run(fmt.Sprintf("operator=%s/version=%s/index=%d", configuration.Operator, configuration.Version, idx), func(t *testing.T) {
+				output := make([]uint64, len(testCase.InputA))
+				expected := testCase.Expected[configuration.Operator]
+				configuration.Func(output, testCase.InputA, testCase.InputB)
+				if !reflect.DeepEqual(output, expected) {
+					t.Errorf("%s gave %v instead of expected %v for inputs %v and %v", runtime.FuncForPC(reflect.ValueOf(configuration.Func).Pointer()).Name(), output, expected, testCase.InputA, testCase.InputB)
+				}
+			})
 		}
 	}
 }
-
-func TestAndSliceGeneric(t *testing.T) { testBitOpSlice(t, andSliceGeneric, OP_AND) }
-func TestAndSliceAsm(t *testing.T)     { testBitOpSlice(t, andSlice, OP_AND) }
-
-func TestOrdSliceGeneric(t *testing.T) { testBitOpSlice(t, orSliceGeneric, OP_OR) }
-func TestOrdSliceAsm(t *testing.T)     { testBitOpSlice(t, orSlice, OP_OR) }
-
-func TestXorSliceGeneric(t *testing.T) { testBitOpSlice(t, xorSliceGeneric, OP_XOR) }
-func TestXorSliceAsm(t *testing.T)     { testBitOpSlice(t, xorSlice, OP_XOR) }
 
 func benchmarkBitOp(b *testing.B, sliceLength int, bitOpFunc func(dest, a, b []uint64)) {
 	b.StopTimer()
@@ -226,27 +218,34 @@ func BenchmarkXorSlice1000(b *testing.B)        { benchmarkBitOp(b, 1000, xorSli
 func BenchmarkXorSlice10000Generic(b *testing.B) { benchmarkBitOp(b, 10000, xorSliceGeneric) }
 func BenchmarkXorSlice10000(b *testing.B)        { benchmarkBitOp(b, 10000, xorSlice) }
 
-var notSliceTestCases = []struct {
-	Input    []uint64
-	Expected []uint64
-}{
-	{[]uint64{}, []uint64{}},
-	{[]uint64{0x1}, []uint64{0xfffffffffffffffe}},
-	{[]uint64{0x1, 0xffff, 0xffff0000, 0x0}, []uint64{0xfffffffffffffffe, 0xffffffffffff0000, 0xffffffff0000ffff, 0xffffffffffffffff}},
-}
+func TestNotSlice(t *testing.T) {
+	testCases := []struct {
+		Input    []uint64
+		Expected []uint64
+	}{
+		{[]uint64{}, []uint64{}},
+		{[]uint64{0x1}, []uint64{0xfffffffffffffffe}},
+		{[]uint64{0x1, 0xffff, 0xffff0000, 0x0}, []uint64{0xfffffffffffffffe, 0xffffffffffff0000, 0xffffffff0000ffff, 0xffffffffffffffff}},
+	}
 
-func testNotSlice(t *testing.T, bitOpFunc func(dest, src []uint64)) {
-	for _, testCase := range notSliceTestCases {
-		output := make([]uint64, len(testCase.Input))
-		bitOpFunc(output, testCase.Input)
-		if !reflect.DeepEqual(output, testCase.Expected) {
-			t.Errorf("%s gave %v instead of expected %v for input %v", runtime.FuncForPC(reflect.ValueOf(bitOpFunc).Pointer()).Name(), output, testCase.Expected, testCase.Input)
-		}
+	for idx, testCase := range testCases {
+		t.Run(fmt.Sprintf("version=generic/index=%d", idx), func(t *testing.T) {
+			output := make([]uint64, len(testCase.Input))
+			notSliceGeneric(output, testCase.Input)
+			if !reflect.DeepEqual(output, testCase.Expected) {
+				t.Errorf("notSliceGeneric() gave %v instead of expected %v for input %v", output, testCase.Expected, testCase.Input)
+			}
+		})
+
+		t.Run(fmt.Sprintf("version=asm/index=%d", idx), func(t *testing.T) {
+			output := make([]uint64, len(testCase.Input))
+			notSlice(output, testCase.Input)
+			if !reflect.DeepEqual(output, testCase.Expected) {
+				t.Errorf("notSliceGeneric() gave %v instead of expected %v for input %v", output, testCase.Expected, testCase.Input)
+			}
+		})
 	}
 }
-
-func TestNotSliceGeneric(t *testing.T) { testNotSlice(t, notSliceGeneric) }
-func TestNotSliceAsm(t *testing.T)     { testNotSlice(t, notSlice) }
 
 func benchmarkNot(b *testing.B, sliceLength int, bitOpFunc func(dest, src []uint64)) {
 	b.StopTimer()
